8ada26dbaa29e4f2c4dde9f0a312217e7a82493c
Meganster <ppro100takk@mail.ru>
2018-11-21 21:44:46 +0300

    Big up

--- components/email_sending_form.py
+++ components/email_sending_form.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import re
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import WebDriverException
@@ -13,10 +14,23 @@ class EmailSendingForm(BaseForm):
     CORRECT_EMAILS = ['adam404pet@gmail.com', 'headmonster3@yandex.ru']
     WRONG_EMAILS = ['sdvskdvnskldv@mail', 'sdv238y2jkdvn']
 
-    def check_recipients(self, emails):
-        span = WebDriverWait(self.driver, 2) \
-            .until(lambda driver: driver.find_element_by_xpath(self.SEND_EMAIL.format(0)))
-        print span.get_attribute('innerHTML')
+    def set_correct_recipients(self):
+        CORRECT_EMAILS = ['adam404pet@gmail.com', 'headmonster3@yandex.ru']
+        emailsStr = ""
+        for i in range(len(self.CORRECT_EMAILS)):
+            emailsStr += CORRECT_EMAILS[i] + "\n"
+        self.add_destionation_email(emailsStr)
+
+    def check_correct_recipients(self):
+        CORRECT_EMAILS = ['adam404pet@gmail.com', 'headmonster3@yandex.ru']
+        for i in range(len(self.CORRECT_EMAILS)):
+            span = WebDriverWait(self.driver, 2) \
+                .until(lambda driver: driver.find_element_by_xpath(self.SEND_EMAIL.format(i)))
+            clean_email_from_ui = re.sub('\s+', '', span.get_attribute('innerHTML'))
+            clean_email_from_ui = re.sub(',', '', clean_email_from_ui)
+            if clean_email_from_ui != CORRECT_EMAILS[i]:
+                return False
+        return True
 
     def add_recipient(self, email_for_add):
         self.add_destionation_email(email_for_add)
--- tests/send_tests/send_tests.py
+++ tests/send_tests/send_tests.py
@@ -8,13 +8,12 @@ class SendTestEmailToMe(BaseSend):
 
         self.email_sending_form.open_writing_letter()
 
-        self.email_sending_form.add_destionation_email('park.test.testovich@mail.ru\n, ppro100takk@mail.ru\n')
+        self.email_sending_form.set_correct_recipients()
         self.email_sending_form.set_subject_email(self.SUBJECT)
         self.email_sending_form.set_message_email(self.TEXT)
         self.email_sending_form.click_send_button()
 
-        self.assertEqual(self.email_sending_form.checkMessageSent(), True)
-
-        self.email_sending_form.check_recipients(2)
+        #self.assertEqual(self.email_sending_form.checkMessageSent(), True)
+        self.assertEqual(self.email_sending_form.check_correct_recipients(), True)
 
         #self.email_sending_form.closeMessageSent()
